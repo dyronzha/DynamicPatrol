1227a421c53a8026c579b5b0d021f7b8eb53db2e
YeeLong <dyron85511@gmail.com>
2020-03-11 21:08:11 +0800

    a* pathfinding

---
+++ DynamicPatrol/Assets/PathFinding/Path.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PathFinder {
+    public class Path
+    {
+        public int curPointID = 1;
+        public readonly Vector3[] lookPoints;
+        public readonly Line[] turnBoundaries;
+        public readonly int finishLineIndex;
+        //public readonly bool[] lookArounds;
+        //public readonly int[] lookNums;
+        //public readonly Vector3[] lookForwards;
+        //public bool CurLookAround
+        //{
+        //    get { return lookArounds[curPointID]; }
+        //}
+        //public int CurLookNum
+        //{
+        //    get { return lookNums[curPointID]; }
+        //}
+        //public Vector3 CurLookFWD
+        //{
+        //    get { return lookForwards[curPointID]; }
+        //}
+
+
+
+        public Path(Vector3[] waypoints, Vector3 startPos, float turnDst) //float stoppingDst
+        {
+            lookPoints = new Vector3[waypoints.Length];
+            turnBoundaries = new Line[lookPoints.Length];
+            finishLineIndex = turnBoundaries.Length - 1;
+
+            Vector2 previousPoint = V3ToV2(startPos);
+            for (int i = 0; i < waypoints.Length; i++)
+            {
+                lookPoints[i] = new Vector3(waypoints[i].x, 0.0f, waypoints[i].z);
+                Vector2 currentPoint = V3ToV2(lookPoints[i]);
+                Vector2 dirToCurrentPoint = (currentPoint - previousPoint).normalized;
+                Vector2 turnBoundaryPoint = (i == finishLineIndex) ? currentPoint : currentPoint - dirToCurrentPoint * turnDst;
+                turnBoundaries[i] = new Line(turnBoundaryPoint, previousPoint - dirToCurrentPoint * turnDst);
+                previousPoint = turnBoundaryPoint;
+            }
+
+            //float dstFromEndPoint = 0;
+            //for (int i = lookPoints.Length - 1; i > 0; i--)
+            //{
+            //    dstFromEndPoint += Vector3.Distance(lookPoints[i], lookPoints[i - 1]);
+            //    if (dstFromEndPoint > stoppingDst)
+            //    {
+            //        canAttckIndex = i;
+            //        break;
+            //    }
+            //}
+        }
+
+        public Path(Vector3[] waypoints, float turnDst) //float stoppingDst
+        {
+            lookPoints = new Vector3[waypoints.Length];
+            turnBoundaries = new Line[lookPoints.Length];
+            finishLineIndex = turnBoundaries.Length - 1;
+
+            Vector2 previousPoint = V3ToV2(waypoints[0]);
+            for (int i = 1; i < waypoints.Length; i++)
+            {
+                lookPoints[i] = new Vector3(waypoints[i].x, 0.0f, waypoints[i].z);
+                Vector2 currentPoint = V3ToV2(lookPoints[i]);
+                Vector2 dirToCurrentPoint = (currentPoint - previousPoint).normalized;
+                Vector2 turnBoundaryPoint = (i == finishLineIndex) ? currentPoint : currentPoint - dirToCurrentPoint * turnDst;
+                turnBoundaries[i] = new Line(turnBoundaryPoint, previousPoint - dirToCurrentPoint * turnDst);
+                previousPoint = turnBoundaryPoint;
+            }
+        }
+
+        //public Path(BloodBond.PatrolPoint[] waypoints, float turnDst) //float stoppingDst
+        //{
+        //    lookPoints = new Vector3[waypoints.Length];
+        //    turnBoundaries = new Line[lookPoints.Length];
+        //    finishLineIndex = turnBoundaries.Length - 1;
+        //    lookArounds = new bool[lookPoints.Length];
+        //    lookNums = new int[lookPoints.Length];
+        //    lookForwards = new Vector3[lookPoints.Length];
+        //    Vector2 previousPoint = V3ToV2(waypoints[0].transform.position);
+
+        //    if (waypoints[0].lookAround) {
+        //        lookArounds[0] = true;
+        //        lookNums[0] = waypoints[0].lookNum;
+        //        lookForwards[0] = waypoints[0].transform.forward;
+        //        lookPoints[0] = waypoints[0].transform.position;
+        //    }
+            
+
+        //    for (int i = 1; i < waypoints.Length; i++)
+        //    {
+        //        lookPoints[i] = new Vector3(waypoints[i].transform.position.x, 0.0f, waypoints[i].transform.position.z);
+        //        Vector2 currentPoint = V3ToV2(lookPoints[i]);
+        //        Vector2 dirToCurrentPoint = (currentPoint - previousPoint).normalized;
+        //        Vector2 turnBoundaryPoint = (i == finishLineIndex) ? currentPoint : currentPoint - dirToCurrentPoint * turnDst;
+        //        turnBoundaries[i] = new Line(turnBoundaryPoint, previousPoint - dirToCurrentPoint * turnDst);
+        //        previousPoint = turnBoundaryPoint;
+
+        //        if (waypoints[i].lookAround)
+        //        {
+        //            lookArounds[i] = true;
+        //            lookNums[i] = waypoints[i].lookNum;
+        //            lookForwards[i] = waypoints[i].transform.forward;
+        //            lookPoints[i] = waypoints[i].transform.position;
+        //        }
+        //    }
+        //}
+
+
+        Vector2 V3ToV2(Vector3 v3)
+        {
+            return new Vector2(v3.x, v3.z);
+        }
+
+        public void DrawWithGizmos()
+        {
+
+            Gizmos.color = Color.black;
+            foreach (Vector3 p in lookPoints)
+            {
+                Gizmos.DrawCube(p + Vector3.up, Vector3.one);
+            }
+
+            Gizmos.color = Color.white;
+            foreach (Line l in turnBoundaries)
+            {
+                l.DrawWithGizmos(10);
+            }
+
+        }
+
+    }
+}
+
+
---
+++ DynamicPatrol/Assets/PathFinding/PathRequestManager.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace PathFinder
+{
+    public class PathRequestManager : MonoBehaviour
+    {
+
+        //Queue<PathRequest> pathRequestQueue = new Queue<PathRequest>();
+        List<PathRequest> pathRequestList = new List<PathRequest>();
+
+        PathRequest currentPathRequest;
+
+        static PathRequestManager instance;
+        PathFinding pathFinding;
+
+        PathFinding[] pathFindings;
+
+        bool isProcessingPath;
+
+        void Awake()
+        {
+            instance = this;
+            pathFinding = GetComponent<PathFinding>();
+        }
+
+        //基礎要求路徑
+        public static PathRequest RequestPath(Vector3 pathStart, Vector3 pathEnd, Action<Vector3[], bool> _successCbk)
+        {
+            PathRequest newRequest = new PathRequest(pathStart, pathEnd, _successCbk);
+            //instance.pathRequestQueue.Enqueue(newRequest);
+            instance.pathRequestList.Add(newRequest);
+            instance.TryProcessNext();
+            //Debug.Log("add new finding path request   " + instance.pathRequestList.IndexOf(newRequest));
+            return newRequest;
+        }
+        //比對舊路徑要求路徑
+        public static PathRequest RequestPath(PathRequest oldRequest, Vector3 pathStart, Vector3 pathEnd, Action<Vector3[], bool> _successCbk)
+        {
+            PathRequest newRequest = new PathRequest(pathStart, pathEnd, _successCbk);
+
+            if (instance.CheckProcessingRequest(oldRequest))   //如果目前正在搜尋且跟舊的一樣回傳null
+            {
+                return null;
+            }
+
+            if (oldRequest != null && instance.pathRequestList.Contains(oldRequest))   //有在排隊中更新為新的request
+            {
+                instance.pathRequestList[instance.pathRequestList.IndexOf(oldRequest)] = newRequest;
+            }
+            else
+            {
+                instance.pathRequestList.Add(newRequest);
+            }
+            instance.TryProcessNext();
+
+            return newRequest;
+
+
+            //instance.pathRequestQueue.Enqueue(newRequest);
+        }
+
+        //特定區域要求路徑
+        public static PathRequest RequestPath(PathFinding pathfinding, PathRequest oldRequest, Vector3 pathStart, Vector3 pathEnd, Action<Vector3[], bool> _successCbk)
+        {
+            PathRequest newRequest = new PathRequest(pathStart, pathEnd, _successCbk, pathfinding);
+
+            if (instance.CheckProcessingRequest(oldRequest))   //如果目前正在搜尋且跟舊的一樣回傳null
+            {
+                return null;
+            }
+
+            if (oldRequest != null && instance.pathRequestList.Contains(oldRequest))   //有在排隊中更新為新的request
+            {
+                instance.pathRequestList[instance.pathRequestList.IndexOf(oldRequest)] = newRequest;
+            }
+            else
+            {
+                instance.pathRequestList.Add(newRequest);
+            }
+            instance.TryProcessNext();
+
+            return newRequest;
+        }
+
+
+        void TryProcessNext()
+        {
+            //if (!isProcessingPath && pathRequestQueue.Count > 0)
+            //{
+            //    currentPathRequest = pathRequestQueue.Dequeue();
+            //    isProcessingPath = true;
+            //    pathFinding.StartFindPath(currentPathRequest.pathStart, currentPathRequest.pathEnd);
+            //}
+            if (!isProcessingPath && pathRequestList.Count > 0)
+            {
+                currentPathRequest = pathRequestList[0];
+                pathRequestList.RemoveAt(0);
+                isProcessingPath = true;
+                pathFinding.StartFindPath(currentPathRequest.pathStart, currentPathRequest.pathEnd);
+            }
+        }
+        void AreaTryProcessNext()
+        {
+            //if (!isProcessingPath && pathRequestQueue.Count > 0)
+            //{
+            //    currentPathRequest = pathRequestQueue.Dequeue();
+            //    isProcessingPath = true;
+            //    pathFinding.StartFindPath(currentPathRequest.pathStart, currentPathRequest.pathEnd);
+            //}
+            if (!isProcessingPath && pathRequestList.Count > 0)
+            {
+                currentPathRequest = pathRequestList[0];
+                pathRequestList.RemoveAt(0);
+                isProcessingPath = true;
+                currentPathRequest.pathfinding.StartFindPath(currentPathRequest.pathStart, currentPathRequest.pathEnd);
+            }
+        }
+
+        bool CheckProcessingRequest(PathRequest request)
+        {  //如果要求是正在進行搜尋回傳true
+            if (isProcessingPath && currentPathRequest == request) return true;
+            else return false;
+        }
+
+        public static void ReplaceRequest(string name, PathRequest request)
+        {
+            instance.pathRequestList[instance.pathRequestList.IndexOf(request)] = null;
+
+            //Debug.Log(name + " cancle finding path request   " + instance.pathRequestList.IndexOf(request));
+            if (instance.pathRequestList.Contains(request)) instance.pathRequestList.Remove(request);
+        }
+        public static void CancleRequest(PathRequest request)
+        {
+            //Debug.Log("  cancle finding path request   " +  instance.pathRequestList.IndexOf(request));
+            if (instance.pathRequestList.Contains(request)) instance.pathRequestList.Remove(request);
+        }
+
+
+        public void FinishedProcessingPath(Vector3[] path, bool success)
+        {
+            //Debug.Log("finisg find path");
+            currentPathRequest.callback(path, success);
+            isProcessingPath = false;
+            TryProcessNext();
+        }
+
+
+        public static void ClearExtendPenalty()
+        {
+            instance.pathFinding.ClearGridExtendPenalty();
+        }
+
+        //public void FailProcessingPath() {
+        //    isProcessingPath = false;
+        //}
+
+        public class PathRequest
+        {
+            public PathFinding pathfinding;
+            public Vector3 pathStart;
+            public Vector3 pathEnd;
+            public Action<Vector3[], bool> callback;
+
+            public PathRequest(Vector3 _start, Vector3 _end, Action<Vector3[], bool> _successCbk)
+            {
+                pathStart = _start;
+                pathEnd = _end;
+                callback = _successCbk;
+            }
+
+            public PathRequest(Vector3 _start, Vector3 _end, Action<Vector3[], bool> _successCbk, PathFinding _pathFinding)
+            {
+                pathStart = _start;
+                pathEnd = _end;
+                callback = _successCbk;
+                pathfinding = _pathFinding;
+            }
+        }
+    }
+}
+
+
