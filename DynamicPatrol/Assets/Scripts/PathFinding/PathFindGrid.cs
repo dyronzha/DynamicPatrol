5abcea76dad99cb9fdb04ae32475347691efe613
dyronzha <dyron85511@gmail.com>
2020-07-16 23:40:34 +0800

    fff

--- DynamicPatrol/Assets/Scripts/PathFinding/PathFindGrid.cs
+++ DynamicPatrol/Assets/Scripts/PathFinding/PathFindGrid.cs
@@ -531,8 +531,10 @@ namespace PathFinder
                         else if (n.Direction == 14) Gizmos.color = new Color(0.5f, 0, 0);
                         else if (n.Direction == 23) Gizmos.color = new Color(1, 0, 1);
                         else if (n.Direction == 24) Gizmos.color = new Color(1, 1, 0);
-                        else if (n.Direction == 26) Gizmos.color = new Color(0.5f, 0, 0.5f);
-                        else if (n.Direction == 27) Gizmos.color = new Color(0.5f, 0.5f, 0f);
+                        else if (n.Direction == 26) Gizmos.color = new Color(0.6f, 0, 0.8f);
+                        else if (n.Direction == 27) Gizmos.color = new Color(0.6f, 0.8f, 0f);
+
+                        if (patrolManager.choosenNodeDic.ContainsKey(new Vector2Int(n.gridX, n.gridY))) Gizmos.color = new Color(0, 1, 1);
                         //if (n.Direction == 11) Gizmos.color = Color.cyan;
                         //else if (n.Direction == 12) Gizmos.color = Color.cyan;
                         //else if (n.Direction == 13) Gizmos.color = Color.cyan;
@@ -554,25 +556,26 @@ namespace PathFinder
                             else if (patrolManager.spreadGrid[n.gridX, n.gridY].dir == new Vector2Int(0, -1)) Gizmos.color = new Color(0.5f, 0, 0);
                             else if (patrolManager.spreadGrid[n.gridX, n.gridY].dir == new Vector2Int(-1, 1)) Gizmos.color = new Color(1, 0, 1);
                             else if (patrolManager.spreadGrid[n.gridX, n.gridY].dir == new Vector2Int(1, 1)) Gizmos.color = new Color(1, 1, 0);
-                            else if (patrolManager.spreadGrid[n.gridX, n.gridY].dir == new Vector2Int(-1, -1)) Gizmos.color = new Color(0.5f, 0, 0.5f);
-                            else if (patrolManager.spreadGrid[n.gridX, n.gridY].dir == new Vector2Int(1, -1)) Gizmos.color = new Color(0.5f, 0.5f, 0f);
+                            else if (patrolManager.spreadGrid[n.gridX, n.gridY].dir == new Vector2Int(-1, -1)) Gizmos.color = new Color(0.6f, 0, 0.8f);
+                            else if (patrolManager.spreadGrid[n.gridX, n.gridY].dir == new Vector2Int(1, -1)) Gizmos.color = new Color(0.6f, 0.8f, 0f);
                         } 
                         //if (patrolManager.spreadGrid[n.gridX, n.gridY].close) Gizmos.color = Color.black;
                         if (patrolManager.choosenNodeDic.ContainsKey(new Vector2Int(n.gridX, n.gridY))) {
 
-                            if (patrolManager.choosenNodeDic[new Vector2Int(n.gridX, n.gridY)].neighbor.Count > 2)
-                            {
-                                if (patrolManager.choosenNodeDic[new Vector2Int(n.gridX, n.gridY)].neighbor.Count == 3) Gizmos.color = new Color(1, 0, 1);
-                                else if (patrolManager.choosenNodeDic[new Vector2Int(n.gridX, n.gridY)].neighbor.Count == 4) Gizmos.color = new Color(0.5f, 0, 0);
-                                else if (patrolManager.choosenNodeDic[new Vector2Int(n.gridX, n.gridY)].neighbor.Count == 5) Gizmos.color = new Color(0, 0, 0.5f);
-                                else if (patrolManager.choosenNodeDic[new Vector2Int(n.gridX, n.gridY)].neighbor.Count == 6) Gizmos.color = new Color(0.2f, 0.2f, 0.2f);
-                                else Gizmos.color = new Color(0, 0, 0);
-                            }
-                            else {
-                                if(patrolManager.choosenNodeDic[new Vector2Int(n.gridX, n.gridY)].neighbor.Count == 2) Gizmos.color = new Color(0, 1, 1);
-                                else if (patrolManager.choosenNodeDic[new Vector2Int(n.gridX, n.gridY)].neighbor.Count == 1) Gizmos.color = new Color(0, 0.5f, 0);
-                                else if (patrolManager.choosenNodeDic[new Vector2Int(n.gridX, n.gridY)].neighbor.Count == 0) Gizmos.color = new Color(0, 0.3f, 0);
-                            } 
+                            if (patrolManager.choosenNodeDic.ContainsKey(new Vector2Int(n.gridX, n.gridY))) Gizmos.color = new Color(0, 1, 1);
+                            //if (patrolManager.choosenNodeDic[new Vector2Int(n.gridX, n.gridY)].neighbor.Count > 2)
+                            //{
+                            //    if (patrolManager.choosenNodeDic[new Vector2Int(n.gridX, n.gridY)].neighbor.Count == 3) Gizmos.color = new Color(1, 0, 1);
+                            //    else if (patrolManager.choosenNodeDic[new Vector2Int(n.gridX, n.gridY)].neighbor.Count == 4) Gizmos.color = new Color(0.5f, 0, 0);
+                            //    else if (patrolManager.choosenNodeDic[new Vector2Int(n.gridX, n.gridY)].neighbor.Count == 5) Gizmos.color = new Color(0, 0, 0.5f);
+                            //    else if (patrolManager.choosenNodeDic[new Vector2Int(n.gridX, n.gridY)].neighbor.Count == 6) Gizmos.color = new Color(0.2f, 0.2f, 0.2f);
+                            //    else Gizmos.color = new Color(0, 0, 0);
+                            //}
+                            //else {
+                            //    if(patrolManager.choosenNodeDic[new Vector2Int(n.gridX, n.gridY)].neighbor.Count == 2) Gizmos.color = new Color(0, 1, 1);
+                            //    else if (patrolManager.choosenNodeDic[new Vector2Int(n.gridX, n.gridY)].neighbor.Count == 1) Gizmos.color = new Color(0, 0.5f, 0);
+                            //    else if (patrolManager.choosenNodeDic[new Vector2Int(n.gridX, n.gridY)].neighbor.Count == 0) Gizmos.color = new Color(0, 0.3f, 0);
+                            //} 
                         }
                        
                         //if (patrolManager.confirmGraphNodeDic.ContainsKey(new Vector2Int(n.gridX, n.gridY)))
@@ -609,7 +612,7 @@ namespace PathFinder
 
                     Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter));
                     Gizmos.color = new Color(0, 0, 0);
-                    Gizmos.DrawWireCube(n.worldPosition, Vector3.one * (nodeDiameter));
+                    //Gizmos.DrawWireCube(n.worldPosition, Vector3.one * (nodeDiameter));
 
                 }
                
